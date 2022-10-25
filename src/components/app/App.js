661ba87f58e53f7d265cbf23416f79f67e84c9bc
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-25 13:41:47 +0300

    feat: add button for randomchar view

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,3 +1,4 @@
+import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
@@ -5,12 +6,24 @@ import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
 
-const App = () => {
+class App extends Component {
+    state = {
+        showRandomChar: true
+    }
+    toggleRandomChar = () => {
+        this.setState((state) => {
+            return {
+                showRandomChar: !state.showRandomChar
+            }
+        })
+    }
+    render() {
     return (
         <div className="app">
             <AppHeader/>
             <main>
-                <RandomChar/>
+                {this.state.showRandomChar ? <RandomChar/> :null}
+                <button onClick={this.toggleRandomChar}>Click me</button>
                 <div className="char__content">
                     <CharList/>
                     <CharInfo/>
@@ -18,7 +31,7 @@ const App = () => {
                 <img className="bg-decoration" src={decoration} alt="vision"/>
             </main>
         </div>
-    )
+    )}
 }
 
 export default App;
