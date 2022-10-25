827ec2078c9114da8ce11c156c103ea1a933044b
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-25 21:50:28 +0300

    feat: add charlist and styles for char item image

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,36 +1,23 @@
-import { Component } from "react";
 import AppHeader from "../appHeader/AppHeader";
 import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 
 import decoration from '../../resources/img/vision.png';
-
-class App extends Component {
-    state = {
-        showRandomChar: true
-    }
-    toggleRandomChar = () => {
-        this.setState((state) => {
-            return {
-                showRandomChar: !state.showRandomChar
-            }
-        })
-    }
-    render() {
+const App = () => {
     return (
         <div className="app">
-            <AppHeader/>
+            <AppHeader />
             <main>
-                {this.state.showRandomChar ? <RandomChar/> :null}
+                <RandomChar />
                 <div className="char__content">
-                    <CharList/>
-                    <CharInfo/>
+                    <CharList />
+                    <CharInfo />
                 </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/>
+                <img className="bg-decoration" src={decoration} alt="vision" />
             </main>
         </div>
-    )}
+    )
 }
 
 export default App;
--- src/services/MarvelService.js
+++ src/services/MarvelService.js
@@ -9,8 +9,8 @@ class MarvelService {
     }
     return await res.json();
   }
-  getAllCharacters = () => {
-    const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+  getAllCharacters = async () => {
+    const res = await this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
     return res.data.results.map(this._transformCharacter);
   }
   getCharacter = async (id) => {
@@ -20,6 +20,7 @@ class MarvelService {
 
   _transformCharacter = (char) => {
     return {
+      id: char.id,
       name: char.name,
       description: char.description,
       thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
