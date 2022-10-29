987d0b54d45ebe035ab8783537662f05ff90f27c
DilbarAkkaya <dilbarmutavalova@gmail.com>
2022-10-30 00:12:41 +0300

    feat: change charlist class to hooks, add fokus

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -44,7 +44,7 @@ const CharList = (props) => {
         setError(true);
     }
     function renderItems(arr) {
-        const items = arr.map((item) => {
+        const items = arr.map((item, i) => {
             let imgStyle = { 'objectFit': 'cover' };
             if (item.thumbnail === 'http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available.jpg') {
                 imgStyle = { 'objectFit': 'unset' };
@@ -55,7 +55,16 @@ const CharList = (props) => {
                     tabIndex={0}
                     key={item.id}
                     ref={el => itemRefs.current[i] = el}
-                    onClick={() => props.onCharSelected(item.id)}>
+                    onClick={() => {
+                        props.onCharSelected(item.id);
+                        focusOnItem(i);
+                    }}
+                    onKeyPress={(e) => {
+                        if (e.key === ' ' || e.key === 'Enter')
+                        props.onCharSelected(item.id);
+                        focusOnItem(i);
+                    }
+                    }>
                     <img src={item.thumbnail} alt={item.name} style={imgStyle} />
                     <div className='char__name'>{item.name}</div>
                 </li>
@@ -70,7 +79,7 @@ const CharList = (props) => {
     const itemRefs = useRef([]);
     const focusOnItem = (id) => {
         itemRefs.current.forEach(item => item.classList.remove('char__item_selected'));
-        itemRefs.current[id].charList.add('char__item_selected');
+        itemRefs.current[id].classList.add('char__item_selected');
         itemRefs.current[id].focus();
     }
 
